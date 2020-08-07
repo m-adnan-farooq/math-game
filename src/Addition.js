e85e88c0cc3bdf3cea6e86bea05d145f71b356da
m-adnan-farooq <m.adnan.far@gmail.com>
2020-08-07 09:11:31 +0500

    Initial Commit

---
+++ src/Addition.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+
+const useStyles1 = makeStyles({
+    root: {
+        textAlign: "center",
+        minWidth: 25,
+        maxWidth:300,
+        margin: 40,
+        backgroundColor: "#F64C72",
+        // boxShadow: '2px 5px rbg(255,255,255)',
+    },
+    pos: {
+        marginBottom: 12,
+        fontWeight:600,
+        color: "#242582",
+    },
+});
+
+export const Addition = ({mathsLevel}) => {
+    const classes1 = useStyles1();
+    const [maths, level] = mathsLevel;
+    const [response, setResponse] = useState()
+    
+    const [number, setNumber] = useState([]);
+    var [score, setScore] = useState(0);
+    
+    useEffect(() => {
+        if(level == '1'){
+            setNumber([Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10)])
+        }
+        else if(level == '2'){
+            setNumber([Math.ceil(Math.random() * 30), Math.ceil(Math.random() * 30)])
+        }
+        else if(level == '3'){
+            setNumber([Math.ceil(Math.random() * 60), Math.ceil(Math.random() * 60)])
+        }
+        else if(level == '4'){
+            setNumber([Math.ceil(Math.random() * 100), Math.ceil(Math.random() * 100)])
+        }
+        else {
+            setNumber([100,100])
+        }
+    },[])
+    
+
+    function checkAnswer() {
+        if (response == number[0] + number[1]) {
+            setScore(score += 1)
+            setNumber([Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10)]);
+            document.getElementById('answer').value = '';
+        }
+    }
+    return (
+        <div>
+            <Typography variant="h4" className='heading'>
+                    <span className='heading'>Maths Game for Kids</span>
+            </Typography>
+            <Card className={classes1.root}>
+                <CardContent>
+                    <Typography variant="h6" className={classes1.pos}>
+                        Level-1 Addition
+                        <span className='score'>Score: {score}</span>
+                        <span className='number'>{number[0]} + {number[1]} = </span>
+                        <Input id='answer' type='text' onChange={(e) => setResponse(e.target.value)} placeholder='Answer' />
+                        <Button className='button' autoFocus={true} variant="contained" color='primary' onClick={checkAnswer}>Submit</Button>
+                    </Typography>
+                </CardContent>
+                </Card>
+        </div>
+    )
+} 
---
+++ src/Main.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import { NativeSelect } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import {Addition} from './Addition';
+
+    var maths;
+    var level;
+    var mathLevel;
+export const Main = () => {
+    const levels = [1, 2, 3, 4, 5];
+    const mathematics = ['Addition', 'Subtraction', 'Multiplication', 'Division'];
+
+    
+
+    const [screen, setScreen] = useState(false);
+
+    const handleChangeMaths = (mathsValue) => {
+        maths = mathsValue;
+    }
+
+    const handleChangeLevel = (levelValue) => {
+        level = levelValue;
+    }
+    const submit = () => {
+        mathLevel = [maths, level];
+        setScreen(true);
+    }
+    if (screen === false)
+        return (
+            <div>
+                <Typography variant="h4" className='heading'>
+                    <span className='heading'>Maths Game for Kids</span>
+                </Typography>
+                <FormControl color='primary'>
+                    <NativeSelect className='selectLevel' defaultValue='' onChange={(e) => handleChangeMaths(e.target.value)}>
+                        <option >Select Game</option>
+                        {mathematics.map((key, index) =>
+                            <option key={index} value={mathematics[index]}>
+                                {mathematics[index]}
+                            </option>
+                        )}
+                    </NativeSelect>
+                </FormControl><FormControl>
+                    <NativeSelect className='selectLevel' defaultValue='' onChange={(e) => handleChangeLevel(e.target.value)}>
+                        <option >Select Level</option>
+                        {levels.map((key, index) =>
+                            <option key={index} value={levels[index]}>
+                                {levels[index]}
+                            </option>
+                        )}
+                    </NativeSelect>
+                </FormControl>
+                <Button variant='contained' className='btn-main' onClick={submit}>Submit</Button>
+            </div>
+        )
+
+    else return(
+        <Addition mathsLevel = {mathLevel}/>
+    )
+}
+
